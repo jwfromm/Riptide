dca0b44fbb9450dfaeb6dbaffc11bea2ef88e644
root <jwfromm@uw.edu>
2018-10-20 02:29:53 +0000

    Added dataset function for loading images directly from directory.

--- riptide/utils/datasets.py
+++ riptide/utils/datasets.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
 import os
+import tensorflow as tf
 from functools import partial
 
 FLAGS = tf.flags.FLAGS
@@ -8,41 +8,72 @@ tf.flags.DEFINE_string('record_path', '/data3/imagenet/tfrecords',
         'Path to tfrecord shards.')
 
 def _get_shard_dataset(record_path, split='train'):
-    pattern = os.path.join(record_path, split + "*")
-    files = tf.data.Dataset.list_files(pattern)
-    return files
+  pattern = os.path.join(record_path, split + "*")
+  files = tf.data.Dataset.list_files(pattern)
+  return files
 
 def _decode_jpeg(image_buffer, size, scope=None):
-    with tf.name_scope(values=[image_buffer], name=scope, default_name='decode_jpeg'):
-        image = tf.image.decode_jpeg(image_buffer, channels=3)
-        image = tf.image.resize_images(image, (size, size))
-        image = tf.image.convert_image_dtype(image, dtype=tf.float32)
-    return image
+  with tf.name_scope(values=[image_buffer], name=scope, default_name='decode_jpeg'):
+    image = tf.image.decode_jpeg(image_buffer, channels=3)
+    image = tf.image.resize_images(image, (size, size))
+    image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+  return image
 
 def _decode_imagenet(proto, size, preprocess):
-    feature_map = {
-        'image/encoded': tf.FixedLenFeature([], dtype=tf.string, default_value=''),
-        'image/class/label': tf.FixedLenFeature([1], dtype=tf.int64, default_value=-1),
-        'image/class/text': tf.FixedLenFeature([], dtype=tf.string, default_value=''),
-    }
+  feature_map = {
+    'image/encoded': tf.FixedLenFeature([], dtype=tf.string, default_value=''),
+    'image/class/label': tf.FixedLenFeature([1], dtype=tf.int64, default_value=-1),
+    'image/class/text': tf.FixedLenFeature([], dtype=tf.string, default_value=''),
+  }
 
-    parsed_features = tf.parse_single_example(proto, feature_map)
-    features = _decode_jpeg(parsed_features['image/encoded'], size)
-    labels = parsed_features['image/class/label']
+  parsed_features = tf.parse_single_example(proto, feature_map)
+  features = _decode_jpeg(parsed_features['image/encoded'], size)
+  labels = parsed_features['image/class/label']
 
-    if preprocess != None:
-        features = preprocess(features)
+  if preprocess != None:
+    features = preprocess(features)
 
-    return features, tf.cast(labels, tf.int32)
+  return features, tf.cast(labels, tf.int32)
 
 def get_imagenet_dataset(batch_size, split='train', preprocess=None, size=224):
-    shard_ds = _get_shard_dataset(FLAGS.record_path, split=split)
-    imagenet_ds = shard_ds.apply(tf.contrib.data.parallel_interleave(
-        tf.data.TFRecordDataset, cycle_length=4, sloppy=True))
-    decode_fn = partial(_decode_imagenet, size=size, preprocess=preprocess)
+  shard_ds = _get_shard_dataset(FLAGS.record_path, split=split)
+  imagenet_ds = shard_ds.apply(tf.contrib.data.parallel_interleave(
+    tf.data.TFRecordDataset, cycle_length=4, sloppy=True))
+  decode_fn = partial(_decode_imagenet, size=size, preprocess=preprocess)
+  imagenet_ds = imagenet_ds.apply(tf.contrib.data.map_and_batch(
+    map_func=decode_fn, batch_size=batch_size, num_parallel_batches=4))
+  if split == 'train':
+    imagenet_ds = imagenet_ds.shuffle(buffer_size=128)
+  imagenet_ds = imagenet_ds.prefetch(batch_size)
+  return imagenet_ds
+
+def _parse_imagefolder_samples(filename, label, preprocess=None):
+    image_string = tf.read_file(filename)
+    image_decoded = tf.image.decode_jpeg(image_string, channels=3)
+    if preprocess is not None:
+        image_decoded = preprocess(image_decoded)
+    return image_decoded, label
+
+def imagefolder_dataset(root, batch_size, split='train', preprocess=None, num_workers=4):
+    valid_extensions = ('.png', '.jpg', '.jpeg')
+    split_dir = os.path.join(root, split)
+    labels_list = os.listdir(split_dir)
+    # Iterate through folders and compose list of files and their label.
+    images = []
+    labels = []
+    for i, label in enumerate(labels_list):
+        files = os.listdir(os.path.join(split_dir, label))
+        for f in files:
+          # Make sure found files are valid images.
+          if f.lower().endswith(valid_extensions):
+            images.append((os.path.join(split_dir, label, f)))
+            labels.append(i)
+    # Now that dataset is populated, parse it into proper tf dataset.
+    decode_fn = partial(_parse_imagefolder_samples, preprocess=preprocess)
+    imagenet_ds = tf.data.Dataset.from_tensor_slices((images, labels))
     imagenet_ds = imagenet_ds.apply(tf.contrib.data.map_and_batch(
-        map_func=decode_fn, batch_size=batch_size, num_parallel_batches=4))
+    map_func=decode_fn, batch_size=batch_size, num_parallel_batches=num_workers))
     if split == 'train':
-        imagenet_ds = imagenet_ds.shuffle(buffer_size=128)
-    imagenet_ds = imagenet_ds.prefetch(batch_size)
+        imagenet_ds = imagenet_ds.shuffle(buffer_size=2*batch_size)
+        imagenet_ds = imagenet_ds.prefetch(batch_size)
     return imagenet_ds
