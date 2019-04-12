Fixed batch size calculation with multiple gpus.

riptide/anneal/models/get_optimizer.py
@@ -3,7 +3,12 @@ import tensorflow as tf
 _NUM_IMAGES = 1281167
 
 
-def adam_piecewise(global_step, batch_size):
+def adam_piecewise(global_step, batch_size, num_gpus):
+    if num_gpus == 0:
+        num_gpus = 1
+
+    batch_size = batch_size * num_gpus
+
     starting_lr = 1e-4
     lr_decay = 0.2
     lr_values = [
@@ -14,14 +19,14 @@ def adam_piecewise(global_step, batch_size):
     step_boundaries = [int(x * steps_per_epoch) for x in epoch_boundaries]
     lr_schedule = tf.compat.v1.train.piecewise_constant_decay(
         global_step, boundaries=step_boundaries, values=lr_values)
-    optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=lr_schedule,
-                                                 epsilon=1e-5)
+    optimizer = tf.compat.v1.train.AdamOptimizer(
+        learning_rate=lr_schedule, epsilon=1e-5)
     return optimizer, lr_schedule
 
 
-def get_optimizer(name, global_step, batch_size):
+def get_optimizer(name, global_step, batch_size, num_gpus=1):
     models = {
         'alexnet': adam_piecewise,
     }
 
-    return models[name](global_step, batch_size)
+    return models[name](global_step, batch_size, num_gpus)

scripts/anneal/train_imagenet.py
@@ -43,29 +43,33 @@ def main(argv):
     op_threads, num_workers = setup_gpu_threadpool(len(FLAGS.gpus.split(',')))
     num_gpus = len(FLAGS.gpus.split(','))
     # Set up the data input functions.
-    train_preprocess = partial(preprocess_image,
-                               height=FLAGS.image_size,
-                               width=FLAGS.image_size,
-                               is_training=True)
-    eval_preprocess = partial(preprocess_image,
-                              height=FLAGS.image_size,
-                              width=FLAGS.image_size,
-                              is_training=False)
+    train_preprocess = partial(
+        preprocess_image,
+        height=FLAGS.image_size,
+        width=FLAGS.image_size,
+        is_training=True)
+    eval_preprocess = partial(
+        preprocess_image,
+        height=FLAGS.image_size,
+        width=FLAGS.image_size,
+        is_training=False)
 
     def train_input_fn():
-        ds = imagerecord_dataset(FLAGS.data_path,
-                                 FLAGS.batch_size,
-                                 is_training=True,
-                                 preprocess=train_preprocess,
-                                 num_workers=num_workers)
+        ds = imagerecord_dataset(
+            FLAGS.data_path,
+            FLAGS.batch_size,
+            is_training=True,
+            preprocess=train_preprocess,
+            num_workers=num_workers)
         return ds.repeat(FLAGS.epochs)
 
     def eval_input_fn():
-        ds = imagerecord_dataset(FLAGS.data_path,
-                                 FLAGS.batch_size,
-                                 is_training=False,
-                                 preprocess=eval_preprocess,
-                                 num_workers=num_workers)
+        ds = imagerecord_dataset(
+            FLAGS.data_path,
+            FLAGS.batch_size,
+            is_training=False,
+            preprocess=eval_preprocess,
+            num_workers=num_workers)
         return ds.repeat(1)
 
     # Set up estimaor model function.
@@ -89,7 +93,7 @@ def main(argv):
 
         global_step = tf.compat.v1.train.get_or_create_global_step()
         optimizer, learning_rate = get_optimizer(FLAGS.model, global_step,
-                                                 FLAGS.batch_size)
+                                                 FLAGS.batch_size, num_gpus)
         loss_fn = tf.keras.losses.SparseCategoricalCrossentropy()
 
         # Track learning rate.
@@ -101,39 +105,41 @@ def main(argv):
         predictions = model(features, training=training)
 
         total_loss = loss_fn(labels, predictions)
-        reg_losses = model.get_losses_for(None) + model.get_losses_for(
-            features)
+        reg_losses = model.get_losses_for(None) + model.get_losses_for(features)
         if reg_losses:
             total_loss += tf.math.add_n(reg_losses)
 
         # Compute training metrics.
-        accuracy = tf.compat.v1.metrics.accuracy(labels=labels,
-                                                 predictions=tf.math.argmax(
-                                                     predictions, axis=-1),
-                                                 name='acc_op')
+        accuracy = tf.compat.v1.metrics.accuracy(
+            labels=labels,
+            predictions=tf.math.argmax(predictions, axis=-1),
+            name='acc_op')
         accuracy_top_5 = tf.compat.v1.metrics.mean(
-            tf.math.in_top_k(predictions=predictions,
-                             targets=tf.reshape(labels, [-1]),
-                             k=5,
-                             name='top_5_op'))
+            tf.math.in_top_k(
+                predictions=predictions,
+                targets=tf.reshape(labels, [-1]),
+                k=5,
+                name='top_5_op'))
         metrics = {'accuracy': accuracy, 'accuracy_top_5': accuracy_top_5}
 
         update_ops = model.get_updates_for(features) + model.get_updates_for(
             None)
         with tf.control_dependencies(update_ops):
-            train_op = optimizer.minimize(total_loss,
-                                          var_list=model.trainable_variables,
-                                          global_step=global_step)
+            train_op = optimizer.minimize(
+                total_loss,
+                var_list=model.trainable_variables,
+                global_step=global_step)
         # Keep track of training accuracy.
         if mode == tf.estimator.ModeKeys.TRAIN:
             tf.compat.v1.summary.scalar('train_accuracy', accuracy[1])
             tf.compat.v1.summary.scalar('train_accuracy_top_5',
                                         accuracy_top_5[1])
 
-        return tf.estimator.EstimatorSpec(mode=mode,
-                                          loss=total_loss,
-                                          train_op=train_op,
-                                          eval_metric_ops=metrics)
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            loss=total_loss,
+            train_op=train_op,
+            eval_metric_ops=metrics)
 
     # Now we're ready to configure our estimator and train.
     # Determine proper name for this model.
@@ -149,16 +155,16 @@ def main(argv):
         intra_op_parallelism_threads=op_threads,
         allow_soft_placement=True)
     session_config.gpu_options.allow_growth = True
-    run_config = tf.estimator.RunConfig(save_summary_steps=500,
-                                        log_step_count_steps=500,
-                                        save_checkpoints_secs=3600,
-                                        train_distribute=strategy,
-                                        session_config=session_config)
-    classifier = tf.estimator.Estimator(model_fn=model_fn,
-                                        model_dir=full_model_path,
-                                        config=run_config)
-    train_spec = tf.estimator.TrainSpec(input_fn=train_input_fn,
-                                        max_steps=None)
+    run_config = tf.estimator.RunConfig(
+        save_summary_steps=500,
+        log_step_count_steps=500,
+        save_checkpoints_secs=3600,
+        train_distribute=strategy,
+        session_config=session_config)
+    classifier = tf.estimator.Estimator(
+        model_fn=model_fn, model_dir=full_model_path, config=run_config)
+    train_spec = tf.estimator.TrainSpec(
+        input_fn=train_input_fn, max_steps=None)
     eval_spec = tf.estimator.EvalSpec(input_fn=eval_input_fn)
     tf.estimator.train_and_evaluate(classifier, train_spec, eval_spec)
 