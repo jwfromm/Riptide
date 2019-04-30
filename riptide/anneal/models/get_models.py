42c49ac762dffb00e139b32b4cf08791ef4c712c
Josh Fromm <jwfromm@airlabsgpu004.rr2.philly.selfhost.corp.microsoft.com>
2019-04-29 18:43:19 -0700

    Fixed for more general model getting.

--- riptide/anneal/models/get_models.py
+++ riptide/anneal/models/get_models.py
@@ -4,10 +4,11 @@ from .alexnet import alexnet
 def get_model(name, **kwargs):
     models = {
         'alexnet': alexnet,
-        'alexnet_sgd': alexnet,
         #'vgg11': vgg.vgg11,
     }
     name = name.lower()
+    if 'alexnet' in name:
+        name = 'alexnet'
     if name not in models:
         raise ValueError("%s Not in supported models.\n\t%s" %
                          (name, '\n\t'.join(sorted(models.keys()))))
