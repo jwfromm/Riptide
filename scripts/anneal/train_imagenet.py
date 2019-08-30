Changed tf2.0 loss scaling to be correct

scripts/anneal/train_imagenet.py
@@ -100,7 +100,7 @@ def main(argv):
         loss_object = tf.keras.losses.SparseCategoricalCrossentropy(reduction=tf.keras.losses.Reduction.NONE)
         def loss_fn(labels, predictions):
             per_example_loss = loss_object(labels, predictions)
-            return tf.nn.compute_average_loss(per_example_loss, global_batch_size=FLAGS.batch_size * num_gpus)
+            return tf.nn.compute_average_loss(per_example_loss, global_batch_size=FLAGS.batch_size)
 
         # Track learning rate.
         tf.compat.v1.summary.scalar('learning_rate', learning_rate)